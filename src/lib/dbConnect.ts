722f0d405919b892c720a95165232741ca9147aa
JAVASCRIPT-LEARNING <shaktivel.elangovan2004@gmail.com>
2025-01-28 18:21:48 +0530

    Fix : Minor Bug resolved

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -14,7 +14,7 @@ async function dbConnect(): Promise<void> {
     try {
         const db = await mongoose.connect(process.env.MONGO_URL || "", {})
         connection.isconnected = db.connections[0].readyState;
-        console.log("Database connected successfully ", db.connection)
+        // console.log("Database connected successfully ", db.connection)
     } catch (error) {
         console.log("Database connection error", error)
         process.exit(1);
